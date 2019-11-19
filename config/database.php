ef37d9e14947c9738a8a107942a96ecd2ba8f990
HENRIQUE RODRIGUES DA CUNHA <henrique.rcunha1@senacsp.edu.br>
2019-11-19 10:05:12 -0300

    Finalizado o arquivo database que estabeleze a conexão com o banco de dados

---
+++ config/database.php
@@ -0,0 +1,33 @@
+<?php
+/*
+A classe database ira permitir a comunicacao com o banco de dados.
+Nesta classe teremos a string de conexão com o banco, bem como:
+- Nome de usúario: "root"
+-Senha: "" -> Vazio neste caso 
+-Banco de dados: dbloja
+-Porta de comunicação: 3306
+-Servidor: Localhost ou IP(Local ou remoto)
+E uma variável para a conexao com o banco que será usada em outros arquivos
+e, portanto, será declarada com o modificador  public
+*/ 
+class Database{
+
+    public $conxao;
+    public function getConnection(){
+        try{
+            $conexao = new PDO("mysql:host=localhost;port:3306;dbname:dbloja","root","");
+            #definir o tipo de caracter para o banco como utf8 que é caracter acentuado
+            $conexao ->exec("set name utf8");
+
+        }
+        catch(PDOException $e){
+            echo"Erro ao tentar estabelecer a conexão com o banco de dados. ".$e->getMessage();
+         
+        }
+        return $conexao;
+    }
+
+}
+
+
+?>
